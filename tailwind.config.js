fc93767ab11c25181fdd20a6ad1068bfe0ed42a9
nayakanwesh <pupunnayak777@gmail.com>
2024-11-04 22:24:19 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
